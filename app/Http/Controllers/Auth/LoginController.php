f78b951612475bcedd14e6cacccdb34df43dab66
monzuralam <monzur707@gmail.com>
2024-02-26 22:08:28 +0600

    login controller & view has been created

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Main view
+     *
+     * @return void
+     */
+    public function index(){
+        return view('login.index');
+    }
+
+    /**
+     * Authenticate login
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function authenticate(Request $request){
+        $data = $request->only('email', 'password');
+        $remember = ! empty( $request->only('remember') ) ? $request->only('remember') : false;
+        if( Auth::attempt( $data, $remember ) ){
+            return redirect()->route('dashboard');
+        }
+    }
+}
